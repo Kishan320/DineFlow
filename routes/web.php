151a81ca1566302141d200bb911809b00d44f8c2
Kishan320 <160992894+Kishan320@users.noreply.github.com>
2026-06-11 14:31:00 +0530

    added routes for clear cache

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 // Serve Vue SPA for all non-API routes
+Route::get('/clear-cache', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+
+    return nl2br(Artisan::output());
+});
 Route::get('/{any?}', function () {
     return view('welcome');
 })->where('any', '.*');
