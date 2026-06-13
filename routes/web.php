853a58c7635daf1b74ddb2a4976b6071e677b9d1
Kishan320 <160992894+Kishan320@users.noreply.github.com>
2026-06-13 21:19:10 +0530

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 // Serve Vue SPA for all non-API routes
-Route::get('/storage-link', function () {
-    Artisan::call('storage:link');
-
-    return 'Storage linked successfully!';
-});
 Route::get('/{any?}', function () {
     return view('welcome');
 })->where('any', '.*');
