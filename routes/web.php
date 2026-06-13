9636d7bfe4770c422388931cc3d00590780695bd
Kishan320 <160992894+Kishan320@users.noreply.github.com>
2026-06-13 21:14:04 +0530

    storage-link cmd added

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 // Serve Vue SPA for all non-API routes
-Route::get('/clear-cache', function () {
-    Artisan::call('optimize:clear');
-    Artisan::call('config:clear');
-    Artisan::call('cache:clear');
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
 
-    return nl2br(Artisan::output());
+    return 'Storage linked successfully!';
 });
 Route::get('/{any?}', function () {
     return view('welcome');
